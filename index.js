80be9fceca13ba803ed6c21a7310e12e34016e8d
Marco Mezzavilla <hub@marcomezzavilla.com>
2021-10-23 10:27:13 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
+const url = "https://dati.venezia.it/sites/default/files/dataset/opendata/livello.json";
+
+async function handler(_req) {
+  const resp = await fetch(url, {
+    headers: {
+      accept: "application/json",
+    },
+  });
+  return new Response(resp.body, {
+    status: resp.status,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
+
+console.log("Listening on http://localhost:8080");
+await listenAndServe(":8080", handler);
